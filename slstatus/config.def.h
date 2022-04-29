0a315403ef2bd27f51245b6589ec12785f5fb142
MokuKen <91607111+MokuKen@users.noreply.github.com>
2022-04-29 13:07:24 +0800

    Update config.def.h

--- slstatus/config.def.h
+++ slstatus/config.def.h
@@ -68,7 +68,7 @@ static const struct arg args[] = {
 	{ run_command, "  %s ", "amixer sget Master | awk -F\"[][]\" '/%/ { print $2 }' | head -n1" },
 	{ battery_perc, "  %s%% ", "BAT0" },
 	{ datetime, "  %s ",		"%F" },
-	{ datetime, "  %s      ",           "%I:%M" },
+	{ datetime, "  %s    ",           "%I:%M" },
 	{ separator, ";", NULL },
 	{ wifi_essid, "  %s ", "wlo1" },
 	{ run_command, "  %s", "ifconfig wlo1 | grep 'TX packets' | awk {'print $6 $7'}" },
